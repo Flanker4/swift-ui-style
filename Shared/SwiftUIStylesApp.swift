fc7c35f243341337610163349d03337cc94e4ce8
Andrii Boiko <aboiko@paloaltonetworks.com>
2022-08-08 01:23:42 +0300

    fix: add project

---
+++ Shared/SwiftUIStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStylesApp.swift
+//  Shared
+//
+//  Created by Andrii Boiko (Contractor) on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
